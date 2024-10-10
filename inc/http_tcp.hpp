ba07de4009277df45c21754d985a15469ab63258
ayushpareek2003 <ayushpareek1980@gmail.com>
2024-10-10 10:40:59 +0530

    header file

---
+++ inc/http_tcp.hpp
@@ -0,0 +1,18 @@
+#ifndef INCLUDED_HTTP_TCP
+#define INCLUDED_HTTP_TCP
+
+namespace http{
+    class TCPServer{
+        public:
+            TCPServer();
+            ~TCPServer();
+
+        private:
+
+
+    };
+}
+
+
+
+#endif
