b0b9e4a237e7d96b4f621fc389aff6c8c8810de2
ayushpareek2003 <ayushpareek1980@gmail.com>
2024-10-12 02:30:22 +0530

    working

--- server.cpp
+++ server.cpp
@@ -1,10 +1,9 @@
-#include <inc\http_tcp.hpp>
+#include "inc/http_tcp.hpp"
 
 int main(){
-    http::TCPServer server=http::TCPServer();
-
-
-    
+    http::TCPServer server=http::TCPServer("127.0.0.1",8080);
+    std::cout<<"Server started"<<std::endl;
+    server.startlisten();
 
     return 0;
 }
