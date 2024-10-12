5e196b7d5f3a773377f8f223e10b08346ee10250
ayushpareek2003 <ayushpareek1980@gmail.com>
2024-10-13 00:35:03 +0530

    final launching file

--- server.cpp
+++ server.cpp
@@ -1,7 +1,13 @@
 #include "inc/http_tcp.hpp"
 
 int main(){
-    http::TCPServer server=http::TCPServer("127.0.0.1",8080);
+    std::string path;
+    std::cout<<"Welcome to TCP server By Ayush Pareek, 11/10/2024"<<std::endl;
+    std::cout<<" "<<std::endl;
+    std::cout<<"Enter path to your Html File"<<std::endl;
+    std::cin>>path;
+
+    http::TCPServer server=http::TCPServer("127.0.0.1",8080,path);
     std::cout<<"Server started"<<std::endl;
     server.startlisten();
 
